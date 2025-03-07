18b1ea7a239f3cd671713db982359738ba076a02
Abishekdevil <abishekcholan07@gmail.com>
2025-03-07 09:51:23 +0530

    completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,59 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-
-import Productcard from './components/productcard.jsx';
+import tv from './assets/tv.jpeg'
+import mobile from './assets/mobile.jpeg';
+import ac from './assets/ac.jpeg'
+import Productcard from './components/productcard'
 
 function App() {
-  const cards = Array.from({ length: 10 });
+  const cards =[
+    {
+      productImage: tv,
+      productName:"Tv",
+      price: '$499',
+    },
+    {
+      productImage: mobile,
+      productName: "Samsung A15 smart phone",
+      price: '$249',
+    },
+    {
+      productImage: ac,
+      productName: "Whirlpool AC",
+      price: '$249',
+    },
+    {
+    productImage: tv,
+    productName:"Tv",
+    price: '$499',
+  },
+  {
+    productImage: mobile,
+    productName: "Samsung A15 smart phone",
+    price: '$249',
+  },
+  {
+    productImage: ac,
+    productName: "Whirlpool AC",
+    price: '$249',
+  },
+  {
+    productImage: tv,
+    productName:"Tv",
+    price: '$499',
+  },
+  {
+    productImage: mobile,
+    productName: "Samsung A15 smart phone",
+    price: '$249',
+  },
+  {
+    productImage: ac,
+    productName: "Whirlpool AC",
+    price: '$249',
+  },
+  ]
   const gridStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(4, 1fr)',
@@ -16,8 +64,8 @@ function App() {
 
   return (
     <div style={gridStyle}>
-      {cards.map((_, index) => (
-        <Productcard key={index} />
+      {cards.map((i, index) => (
+        <Productcard key={index} productName={i.productName} Image={i.productImage} Price={i.price}/>
       ))}
     </div>
   );
--- src/components/productcard.jsx
+++ src/components/productcard.jsx
@@ -1,1 +1,22 @@
 // write product card here
+import React from 'react';
+import '../App.css';
+import Button from '../components/button'
+
+const productcard=({Image,ProductName,Price})=>{
+    return(
+        <div>
+          <div className="box">
+            <img src={Image} alt='image' />
+          </div>
+          <div className="name">
+          <p><strong>Product Name :</strong>{ProductName}</p>
+          </div>
+          <div className="price">
+          <p><strong>Price :</strong>{Price}</p>
+          </div>
+          <Button />
+        </div>
+    )
+}
+export default productcard;
